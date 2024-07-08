1c9175230a7808bd739ce44c4df0bb86cc9b8654
LupisCor <doliveri575@gmail.com>
2024-07-08 19:20:37 -0400

    update main.py to output sorted list of characters

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_dict = get_num_characters(text)
-    print(f"{num_words} words found in the document")
-    print(char_dict)
+    char_list = dict_to_list(char_dict)
+    char_list.sort(reverse=True, key=sort_on)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")
+    for i in range(0, len(char_list)):
+        print(f"The '{char_list[i]['char']}' character was found {char_list[i]['num']} times")
+    print("--- End report ---")
+
+
+def sort_on(dict):
+    return dict["num"]
+
+def dict_to_list(char_dict):
+    char_list = []
+    for char in char_dict:
+        count = char_dict[char]
+        if char.isalpha():
+            char_list.append({"char": char, "num": count})
+    return(char_list)
 
 
 def get_num_characters(text):
